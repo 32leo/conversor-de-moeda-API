98ba4d5aff2bca4d5ebc46c9591225d031d462bb
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-11-01 08:40:51 -0300

    realiza as alteracoes

--- API.js
+++ API.js
@@ -3,4 +3,21 @@ const apiURL = ` https://v6.exchangerate-api.com/v6/${apiKey}750e5cbab3fc2522ab7
 
 
 // função apara consulta de taxa de cambio de API
-async function getExgaente(daMoeda,paraMoeda){} 
+// #########################################################################################################################################
+async function getExgaente(daMoeda,paraMoeda){
+    try{ 
+        const response = await fetch(`${apiURL}${daMoeda}`); 
+        const data =response.json();
+        if(data.reslt=== "succes"){
+            return data.conversion_rates[paraMoeda];
+            
+        }else{
+            throw new error('erro ao buscar taxa de cambio');
+         }
+
+    }catch (error){
+        console.error("erro:", error);
+        return null;
+    }
+
+    }
