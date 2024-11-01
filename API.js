70f9d17d3537da7550c2ef87f5d28371e3c14194
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-11-01 08:27:47 -0300

    realizaas alteracoes

---
+++ API.js
@@ -0,0 +1,6 @@
+const apiKey = '750e5cbab3fc2522ab79b19c'
+const apiURL = ` https://v6.exchangerate-api.com/v6/${apiKey}750e5cbab3fc2522ab79b19c/latest/USD`;
+
+
+// função apara consulta de taxa de cambio de API
+async function getExgaente(daMoeda,paraMoeda){} 
